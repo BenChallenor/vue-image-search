5d4b7089104d710f791508b324a56d288f6e0c9a
Ben <ben.challenor@btopenworld.com>
2018-09-30 16:13:56 +0100

    Add search form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template lang="html">
   <div id='app'>
-    <h1>Vue Image Search</h1>
+    <h1>{{title}}</h1>
+
+    <form>
+      <label for="searchTerm">Search Term</label>
+      <input class="u-full-width" type="text" id="searchTerm" name="searchTerm">
+      <button class="button-primary" type="submit">Search</button>
+    </form>
 
   </div>
 </template>
 
 <script>
 export default {
-
   name: 'app',
+  data() {
+    return {
+      title: 'Vue Image Search',
+    };
+  },
 };
 </script>
 
 <style lang="css">
-
 body {
   width: 80%;
   margin: 2em auto 0 auto;
